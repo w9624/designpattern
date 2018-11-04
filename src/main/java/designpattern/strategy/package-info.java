9d74f655d477f9b7a76f975845bef6a767a02b0d
w9624 <wl7426@163.com>
2018-11-04 22:22:15 +0800

    策略模式

--- src/main/java/designpattern/strategy/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * @author wangrz
- * 解决问题：封装变化（程序中的算法）
- * 实现：使用context来完成对strategy创建，将strategy的不同实现进行分装
- * 优点：算法自由切换；避免多个条件语句；易于扩展
- * 缺点：策略类会增加；所有策略类对外暴露
- */
-package designpattern.strategy;
